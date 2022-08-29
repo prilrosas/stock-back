58c9d78b6ca834152320374b9b0980f6b501035a
Priscila <prilrosas@gmail.com>
2022-08-29 10:17:28 -0400

    Comentando codigos

--- db.js
+++ db.js
@@ -24,6 +24,28 @@ async function insertStock(stock){
     const sql = 'INSERT INTO stock (close, date, high, low,open, volume) VALUES (?,?,?,?,?,?);';
     const values = [stock.close, stock.date, stock.high, stock.low,stock.open, stock.volume];
     return await conn.query(sql, values);
-}
+};
 
-module.exports = {selectStock,insertStock}
+
+async function selectStockPolicy(){
+    const conn = await connect();
+    const [rows] = await conn.query('SELECT * FROM stock_policy;');
+    return rows;
+};
+
+async function insertStockPolicy(data){
+    const conn = await connect();
+    const sql = 'insert into stock_policy (critico_menor_que,bom_menor_que,bom_maior_que,otimo_maior_que)  values(?,?,?,?)';
+    const values = [data.critico_menor_que, data.bom_maior_que, data.bom_menor_que, data.otimo_maior_que];
+    return await conn.query(sql, values);
+};
+
+async function updateStockPolicy(data){
+    const conn = await connect();
+    const sql = 'update stock_policy set critico_menor_que = ? ,bom_menor_que = ? ,bom_maior_que = ? ,otimo_maior_que = ? where idstock_policy=?';
+    const values = [data.critico_menor_que,data.bom_menor_que, data.bom_maior_que,  data.otimo_maior_que, data.idstock_policy];
+    return await conn.query(sql, values);
+};
+
+// Funções disponiveis para outros arquivos
+module.exports = {selectStock,insertStock,selectStockPolicy,insertStockPolicy,updateStockPolicy}
--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-const express = require('express');
+const express = require('express'); 
 const bodyParser = require('body-parser');
-const { response } = require('express');
-const fs = require('fs');
-
-// Upload de arquivo
-const busboy = require("connect-busboy");
-// Leitura csv
-const { parse } = require("csv-parse");
-
+const busboy = require("connect-busboy");  // Upload de arquivo
+const { parse } = require("csv-parse");   // Leitura csv
 const db = require("./db");
 const cors = require('cors');
 const app = express();
@@ -23,71 +17,105 @@ app.use(cors({
 app.get("/stock", (request, response) => {
     const listar = async function () {
         const results = await db.selectStock();
-        for (const item of results) { 
-            let status="";
-            const vol= item.volume;
-            if (vol>140){
-                item["status"]="otimo";
-            }else if (vol>100 && vol<140){
-                item["status"]="bom";
-            }else if(vol<100){
-                item["status"]="critico";
-            }           
-          }
+        for (const item of results) {
+            let status = "";
+            const vol = item.volume;
+            if (vol > 140) {
+                item["status"] = "otimo";
+            } else if (vol > 100 && vol < 140) {
+                item["status"] = "bom";
+            } else if (vol < 100) {
+                item["status"] = "critico";
+            }
+        }
         response.status(200).send(results);
     };
     listar();
 });
 
-app.post("/stock", (req, res) => {
-    const response = req.body;
-    response['id'] =
-        res.status(200).send(response);
-});
+
 
 
 app.use(busboy());
 app.post("/stock/upload", function (req, res) {
-    if (req.busboy) {
-        req.busboy.on("file", function (fieldName, fileStream, fileName, encoding, mimeType) {
-            fileStream.pipe(parse({ delimiter: ",", from_line: 2 }))
-                .on("data", function (row) {
-
-                    // Date,Open,High,Low,Close,Volume
-                    let dt = new Date(row[0]);
-                    let strDt = dt.toISOString().split("T")[0];
-
-                    let item = {
-                        "close": row[4],
-                        "date": strDt,
-                        "high": row[2],
-                        "low": row[3],
-                        "open": row[1],
-                        "volume": row[5]
-                     };
-
-                    const inserir = async function (stock_json) {
-                        const result = await db.insertStock(stock_json);
-                    };
-                    console.log(item)
-                    inserir(item);
-                })
-                .on("end", function () {
-                    console.log("finished");
-                    res.send('upload succeeded!');
-                })
-                .on("error", function (error) {
-                    console.log("Erro");
-                    console.log(error.message);
-                    res.status(500).send('Arquivo invalido:'+error.message);
-                });
+    try {
+        if (req.busboy) {
+            req.busboy.on("file", function (fieldName, fileStream, fileName, encoding, mimeType) {
 
 
-        });
-        return req.pipe(req.busboy);
+
+                fileStream.pipe(parse({ delimiter: ",", from_line: 2 }))
+                    .on("data", function (row) {
+                        try {
+                            // Date,Open,High,Low,Close,Volume
+                            let dt = new Date(row[0]);
+                            let strDt = dt.toISOString().split("T")[0];
+
+                            let item = {
+                                "close": row[4],
+                                "date": strDt,
+                                "high": row[2],
+                                "low": row[3],
+                                "open": row[1],
+                                "volume": row[5]
+                            };
+
+                            const inserir = async function (stock_json) {
+                                const result = await db.insertStock(stock_json);
+                            };
+                            console.log(item)
+                            inserir(item);
+                        } catch (e) {
+                            // declarações para manipular quaisquer exceções
+                        };
+                    })
+                    .on("end", function () {
+                        console.log("finished");
+                        res.send('upload succeeded!');
+                    })
+                    .on("error", function (error) {
+                        console.log("Erro");
+                        res.status(500).send('Arquivo invalido:'); // passa o objeto de exceção para o manipulador de erro
+                    });
+
+
+            });
+            return req.pipe(req.busboy);
+        }
     }
-    //Something went wrong -- busboy was not loaded
+    catch (e) {
+        // declarações para manipular quaisquer exceções
+        res.status(500).send('Arquivo invalido:' + error.message); // passa o objeto de exceção para o manipulador de erro
+    }
+
 });
+
+app.get("/stock_policy", (request, response) => {
+    const listar = async function () {
+        const results = await db.selectStockPolicy();
+
+        response.status(200).send(results);
+    };
+    listar();
+});
+
+
+
+app.put("/stock_policy", (request, response) => {
+    const body = request.body;
+
+    console.log(body)
+    const atualizar = async function (data) {
+        const results = await db.updateStockPolicy(data)
+
+        response.status(200).send("sucesso");
+    };
+    atualizar(body);
+
+
+
+});
+
 
 app.use(busboy({ immediate: true }));
 
